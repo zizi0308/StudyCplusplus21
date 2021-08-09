bd0755c2c76a5f4a5e714f47c48f135c89321e47
JO HUI JI <whgmlwl222@naver.com>
2021-08-09 15:56:33 +0900

    20210809 문제푸는 중 커밋

--- 문제풀이/1110.cpp
+++ 문제풀이/1110.cpp
@@ -3,7 +3,30 @@
 int main()
 {
 	int N = 0;
+	int a = 0;
+	int b = 0;
+	int count = 1;
+	int res = 0;
+	
+	scanf("%d", &N);
+	a = N / 10;
+	b = N % 10;
+	if (a + b <= 10) res = a + b;
+	else res = b * 10 + (a + b);
+	while (1)
+	{
+		a = res / 10;
+		if (a + b <= 10) res = a * 10;
+		else
+		{
+			b = res % 10;
+			res = b * 10 + (a + b) % 10;
+		}
+		count++;
 
-
+		if (N == res) break;
+	}
+	printf("%d", count);
+	
 	return 0;
 }
